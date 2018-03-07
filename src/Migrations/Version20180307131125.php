fe78dfe45849cf7d3b45d8997d8a7c48fd6bec52
Axel Venet <kern046@gmail.com>
2018-03-07 14:18:42 +0100

    Add missing project field updated_at

---
+++ src/Migrations/Version20180307131125.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180307131125 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE developtech_agility__projects ADD updated_at DATETIME');
+        $this->addSql('UPDATE developtech_agility__projects SET updated_at = created_at WHERE 1');
+        $this->addSql('ALTER TABLE developtech_agility__projects CHANGE updated_at updated_at DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE developtech_agility__projects DROP updated_at');
+    }
+}

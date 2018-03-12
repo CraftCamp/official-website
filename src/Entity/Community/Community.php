9739fee145bf08b6dcc42f2b0fae0cf774efb390
Axel Venet <kern046@gmail.com>
2018-03-12 15:10:17 +0100

    Implement entities and migration scripts for communities and pictures

---
+++ src/Entity/Community/Community.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity\Community;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use App\Entity\Picture;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="communities__community")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Community implements \JsonSerializable
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     **/
+    protected $id;
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     **/
+    protected $name;
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    protected $slug;
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Picture", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=true)
+     **/
+    protected $picture;
+    /**
+     * @ORM\Column(type="datetime")
+     **/
+    protected $createdAt;
+    /**
+     * @ORM\Column(type="datetime")
+     **/
+    protected $updatedAt;
+    
+    /**
+     * @ORM\PrePersist()
+     */
+    public function prePersist()
+    {
+        $this->createdAt = $this->updatedAt = new \DateTime();
+    }
+    
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function preUpdate()
+    {
+        $this->updatedAt = new \DateTime();
+    }
+    
+    /**
+     * @param int $id
+     * @return \Community
+     */
+    public function setId(int $id): Community
+    {
+        $this->id = $id;
+        
+        return $this;
+    }
+    
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    
+    /**
+     * @param string $name
+     * @return Community
+     */
+    public function setName(string $name): Community
+    {
+        $this->name = $name;
+        
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    
+    /**
+     * @param string $slug
+     * @return Community
+     */
+    public function setSlug(string $slug): Community
+    {
+        $this->slug = $slug;
+        
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+    
+    /**
+     * @param Picture $picture
+     * @return Community
+     */
+    public function setPicture(Picture $picture): Community
+    {
+        $this->picture = $picture;
+        
+        return $this;
+    }
+    
+    /**
+     * @return Picture
+     */
+    public function getPicture(): Picture
+    {
+        return $this->picture;
+    }
+    
+    /**
+     * @param \DateTime $createdAt
+     * @return Community
+     */
+    public function setCreatedAt(\DateTime $createdAt): Community
+    {
+        $this->createdAt = $createdAt;
+        
+        return $this;
+    }
+    
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+    
+    /**
+     * @param \DateTime $updatedAt
+     * @return Community
+     */
+    public function setUpdatedAt(\DateTime $updatedAt): Community
+    {
+        $this->updatedAt = $updatedAt;
+        
+        return $this;
+    }
+    
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+    
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'picture' => $this->picture,
+            'created_at' => $this->createdAt,
+            'updated_at' => $this->updatedAt
+        ];
+    }
+}
---
+++ src/Entity/Picture.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="pictures")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Picture
+{
+    const UPLOAD_DIR = 'images/uploads';
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $path;
+    /**
+     * @var UploadedFile
+     */
+    protected $file;
+    /** @var resource **/
+    protected $temp;
+
+    /**
+     * @param int $id
+     * @return \App\Model\PictureModel
+     */
+    public function setId(int $id): Picture
+    {
+        $this->id = $id;
+        
+        return $this;
+    }
+    
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    
+    /**
+     * @param string $name
+     * @return \App\Model\PictureModel
+     */
+    public function setName(string $name): Picture
+    {
+        $this->name = $name;
+        
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    
+    /**
+     * @param string $path
+     * @return \App\Model\PictureModel
+     */
+    public function setPath(string $path): Picture
+    {
+        $this->path = $path;
+        
+        return $this;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+    
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     * @return Picture
+     */
+    public function setFile(UploadedFile $file = null): Picture
+    {
+        $this->file = $file;
+        // check if we have an old image path
+        if (is_file($this->getAbsolutePath())) {
+            // store the old name to delete after the update
+            $this->temp = $this->getAbsolutePath();
+            $this->path = null;
+        } else {
+            $this->path = 'initial';
+        }
+        return $this;
+    }
+    
+    /**
+     * @return UploadedFile
+     */
+    public function getFile(): UploadedFile
+    {
+        return $this->file;
+    }
+    
+    
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null !== $this->file) {
+            $this->path = "{$this->name}.{$this->file->guessExtension()}";
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->file) {
+            return;
+        }
+
+        // check if we have an old image
+        if (isset($this->temp)) {
+            // delete the old image
+            unlink($this->temp);
+            // clear the temp image path
+            $this->temp = null;
+        }
+        // you must throw an exception here if the file cannot be moved
+        // so that the entity is not persisted to the database
+        // which the UploadedFile move() method does
+        $this->file->move(
+            $this->getUploadRootDir(),
+            $this->path
+        );
+        $this->setFile(null);
+    }
+
+    /**
+     * @ORM\PreRemove()
+     */
+    public function storeFilenameForRemove()
+    {
+        $this->temp = $this->getAbsolutePath();
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if (isset($this->temp)) {
+            unlink($this->temp);
+        }
+    }
+    
+    /**
+     * @return string
+     */
+    public function getAbsolutePath()
+    {
+        return
+            (null === $this->path)
+            ? null
+            : $this->getUploadRootDir().'/'.$this->id.'.'.$this->path
+        ;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getWebPath()
+    {
+        return 
+            (null === $this->path)
+            ? null
+            : $this->getUploadDir().'/'.$this->path
+        ;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getUploadDir(): string
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return '/' . self::UPLOAD_DIR;
+    }
+    
+    public function getUploadRootDir(): string
+    {
+        return __DIR__ . '/../../public/' . self::UPLOAD_DIR;
+    }
+    
+    /**
+     * @return array
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'path' => $this->getWebPath()
+        ];
+    }
+    
+}

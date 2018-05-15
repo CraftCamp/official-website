bb5770ac10a380663162a74849c65711563b5700
Axel Venet <kern046@gmail.com>
2018-05-15 19:07:32 +0200

    Implement tests for dashboard communities module

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -20,10 +19,10 @@ class MemberController extends Controller
 {
     /**
      * @Route("/members/dashboard", name="member_dashboard", methods={"GET"})
-     * @IsGranted("ROLE_USER")
      */
     public function dashboardAction(CommunityMemberManager $communityMemberManager)
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
         return $this->render('members/dashboard.html.twig', [
             'communities' => $communityMemberManager->getMemberCommunities($this->getUser()),
         ]);
@@ -40,14 +39,11 @@ class MemberController extends Controller
     /**
      * @Route("/members", name="member_creation", methods={"POST"})
      */
-    public function createMember(Request $request)
+    public function createMember(Request $request, UserManager $userManager, AuthenticationManager $authenticationManager)
     {
-        $member = $this->get(UserManager::class)->createUser(
-            $request->request->all(),
-            Member::TYPE_MEMBER
-        );
+        $member = $userManager->createUser($request->request->all(), Member::TYPE_MEMBER);
         
-        $this->get(AuthenticationManager::class)->authenticate($request, $member);
+        $authenticationManager->authenticate($request, $member);
         
         return new JsonResponse($member, 201);
     }
---
+++ tests/Controller/MemberControllerTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tests\Controller;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+use App\Entity\User\Member;
+
+use App\DataFixtures\ORM\LoadOrganizationData;
+use App\DataFixtures\ORM\Community\LoadCommunityData;
+use App\DataFixtures\ORM\User\LoadActivationLinkData;
+use App\DataFixtures\ORM\User\LoadProductOwnerData;
+
+class MemberControllerTest extends WebTestCase
+{
+    public function setUp()
+    {
+        $this->loadFixtures([
+            LoadActivationLinkData::class,
+            LoadOrganizationData::class,
+            LoadProductOwnerData::class,
+            LoadCommunityData::class,
+        ]);
+    }
+    
+    public function testDashboardAction()
+    {
+        $client = $this->makeClient(true);
+        
+        $crawler = $client->request('GET', '/members/dashboard');
+        
+        $this->assertStatusCode(200, $client);
+        $this->assertContains('Mes communautés', $crawler->filter('#communities-card > header > h3')->text());
+    }
+    
+    public function testDashboardActionWithAnonymousToken()
+    {
+        $client = $this->makeClient();
+        
+        $client->request('GET', '/members/dashboard');
+        
+        $this->assertStatusCode(302, $client);
+    }
+    
+    public function testNewAction()
+    {
+        $client = $this->makeClient(true);
+        
+        $crawler = $client->request('GET', '/members/new');
+        
+        $this->assertContains('Rejoindre la communauté', $crawler->filter('#registration > header > h3')->text());
+    }
+    
+    public function testCreateAction()
+    {
+        $client = $this->makeClient(true);
+        $client->request('POST', '/members', [], [], [
+            'CONTENT_TYPE' => 'application/json'
+        ], json_encode([
+            'username' => 'Mickael',
+            'email' => 'mickael@example.org',
+            'password' => 'test',
+            'password_confirmation' => 'test'
+        ]));
+        
+        $this->assertEquals(201, $client->getResponse()->getStatusCode());
+        
+        $content = $client->getResponse()->getContent();
+        
+        $this->assertJson($content);
+        
+        $data = json_decode($content, true);
+        
+        $this->assertEquals(3, $data['id']);
+        $this->assertEquals('Mickael', $data['username']);
+        $this->assertFalse($data['is_enabled']);
+        $this->assertEquals(Member::TYPE_MEMBER, $data['type']);
+    }
+    
+    public function testCreateActionWithWrongPassword()
+    {
+        $client = $this->makeClient(true);
+        $client->request('POST', '/members', [], [], [
+            'CONTENT_TYPE' => 'application/json'
+        ], json_encode([
+            'username' => 'Mickael',
+            'email' => 'mickael@example.org',
+            'password' => 'test',
+            'password_confirmation' => 'wrong'
+        ]));
+        
+        $this->assertEquals(400, $client->getResponse()->getStatusCode());
+        
+        $content = $client->getResponse()->getContent();
+        
+        $this->assertJson($content);
+        
+        $data = json_decode($content, true);
+        
+        $this->assertEquals('users.password_mismatch', $data['message']);
+    }
+}

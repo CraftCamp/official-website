726cded9e4327b1209c85afabce68675fd511ef9
Axel Venet <kern046@gmail.com>
2018-06-29 09:48:46 +0200

    Implement vote form in project poll view

--- public/js/projects.js
+++ public/js/projects.js
@@ -31,3 +31,14 @@ const putProjectDetails = slug => fetch(`/projects/${slug}/details`, {
 .then(data => {
     window.location = `/projects/${slug}/workspace`;
 });
+
+const showVoteForm = type => {
+    document.querySelector('#vote-first-step').style.display = 'none';
+    document.querySelector(`#vote-${type}`).style.display = 'block';
+};
+
+const hideVoteForm = type => {
+    document.querySelector('#vote-first-step').style.display = 'block';
+    document.querySelector('#vote-positive').style.display = 'none';
+    document.querySelector('#vote-negative').style.display = 'none';
+};

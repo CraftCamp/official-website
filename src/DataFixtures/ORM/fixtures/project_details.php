c360004fe58e5ca4e9888a92b8421557c114f95a
Axel Venet <kern046@gmail.com>
2018-08-14 15:32:10 +0200

    Fix the fixtures

--- src/DataFixtures/ORM/fixtures/project_details.php
+++ src/DataFixtures/ORM/fixtures/project_details.php
@@ -18,5 +18,11 @@ return [
         'need' => 'Une plateforme communautaire capable d\'offrir des outils de gestion de projet aux associations bénéficiaires de notre service bénévole',
         'target' => 'Les associations ainsi que les étudiants et personnes en reconversion professionnelle, ou encore les professionnels souhaitant avoir un rôle de coach',
         'goal' => 'Que les associations puissent obtenir des outils numériques, et les apprenants décrocher un premier emploi'
+    ],
+    [
+        'project' => 'bloodline',
+        'need' => 'Une application pour les donneurs de sang pour que les professionnels de santé puissent notifier uniquement les groupes sanguins recherchés.',
+        'target' => 'Les donneurs de sang ainsi que les établissements de santé',
+        'goal' => 'Qu\'un hôpital puisse être rapidement fourni en réserves de sang par les donneurs des groupes recherchés.'
     ]
 ];

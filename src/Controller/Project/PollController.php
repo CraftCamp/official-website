715f9412974d9a1b15528983613a1f2695cb57a4
Axel Venet <kern046@gmail.com>
2018-08-14 11:06:08 +0200

    Implement poll results processing webservice restricted to scheduler microservice

--- src/Controller/Project/PollController.php
+++ src/Controller/Project/PollController.php
@@ -29,7 +29,7 @@ class PollController extends Controller
     /**
      * @Route("/projects/{slug}/polls", name="create_project_poll", methods={"POST"})
      */
-    public function createPoll(ProjectManager $projectManager, DetailsManager $detailsManager, PollManager $pollManager, string $slug)
+    public function create(ProjectManager $projectManager, DetailsManager $detailsManager, PollManager $pollManager, string $slug)
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
         if (($project = $projectManager->get($slug)) === null) {
@@ -41,7 +41,7 @@ class PollController extends Controller
         if (($details = $detailsManager->getCurrentProjectDetails($project)) === null) {
             throw new BadRequestHttpException('projects.polls.not_ready');
         }
-        $poll = $pollManager->createPoll($project, $details);
+        $poll = $pollManager->create($project, $details);
         return $this->redirectToRoute('get_poll', [
             'slug' => $slug,
             'id' => $poll->getId()
@@ -51,7 +51,7 @@ class PollController extends Controller
     /**
      * @Route("/projects/{slug}/polls/{id}", name="get_poll", methods={"GET"})
      */
-    public function getPoll(ProjectManager $projectManager, PollManager $pollManager, DetailsManager $detailsManager, VoteManager $voteManager, string $slug, int $id)
+    public function find(ProjectManager $projectManager, PollManager $pollManager, DetailsManager $detailsManager, VoteManager $voteManager, string $slug, int $id)
     {
         if (($project = $projectManager->get($slug)) === null) {
             throw new NotFoundHttpException('projects.not_found');
@@ -85,5 +85,20 @@ class PollController extends Controller
             $request->request->get('is_positive'),
             $request->request->get('choice')
         ), 201);
+    }
+    
+    /**
+     * @Route("/projects/{slug}/polls/{id}/close", name="close_project_poll", methods={"PUT"})
+     */
+    public function close(PollManager $pollManager, VoteManager $voteManager, Request $request, int $id)
+    {
+        if ($request->getHttpHost() !== 'craftcamp_website') {
+            throw new AccessDeniedHttpException('projects.access_denied');
+        }
+        if (($poll = $pollManager->get($id)) === null) {
+            throw new NotFoundHttpException('projects.votes.not_found');
+        }
+        $pollManager->processResults($poll, $voteManager->getPollVotes($poll));
+        return null;
     }
 }
--- src/Manager/Project/PollManager.php
+++ src/Manager/Project/PollManager.php
@@ -5,7 +5,11 @@ namespace App\Manager\Project;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-use App\Event\Project\NewPollEvent;
+use App\Event\Project\{
+    AcceptedPollEvent,
+    NewPollEvent,
+    RejectedPollEvent
+};
 
 use App\Entity\Project\{
     Details,
@@ -13,23 +17,28 @@ use App\Entity\Project\{
     Project
 };
 
+use App\Gateway\SchedulerGateway;
+
 class PollManager
 {
     /** @var EntityManagerInterface **/
     protected $em;
     /** @var EventDispatcherInterface **/
     protected $eventDispatcher;
+    /** @var SchedulerGateway **/
+    protected $scheduler;
     /** @var string **/
     protected $pollDuration;
     
-    public function __construct(EntityManagerInterface $em, EventDispatcherInterface $eventDispatcher, string $pollDuration)
+    public function __construct(EntityManagerInterface $em, EventDispatcherInterface $eventDispatcher, SchedulerGateway $scheduler, string $pollDuration)
     {
         $this->em = $em;
         $this->eventDispatcher = $eventDispatcher;
+        $this->scheduler = $scheduler;
         $this->pollDuration = $pollDuration;
     }
     
-    public function createPoll(Project $project, Details $details): Poll
+    public function create(Project $project, Details $details): Poll
     {
         $poll =
             (new Poll())
@@ -41,6 +50,12 @@ class PollManager
         $project->setApprovalPoll($poll);
         $this->em->persist($poll);
         $this->em->flush();
+        $this->scheduler->addTask(
+            'PUT',
+            "http://craftcamp_website/projects/{$poll->getProject()->getSlug()}/polls/{$poll->getId()}/close",
+            "poll-{$poll->getId()}-close",
+            $poll->getEndedAt()
+        );
         $this->eventDispatcher->dispatch(NewPollEvent::NAME, new NewPollEvent($poll));
         return $poll;
     }
@@ -49,7 +64,7 @@ class PollManager
     {
         return $this->em->getRepository(Poll::class)->find($id);
     }
-    
+ 
     public function getCurrentProjectPoll(Project $project): ?Poll
     {
         $results = $this->em->getRepository(Poll::class)->findBy([
@@ -61,5 +76,29 @@ class PollManager
             return null;
         }
         return $results[0];
+    }
+    
+    public function processResults(Poll $poll, array $votes)
+    {
+        $count = 0;
+        $votes = $this->getPollVotes($poll);
+        $nbVotes = count($votes);
+        foreach ($votes as $vote) {
+            if ($vote->getIsPositive()) {
+                $count++;
+            }
+        }
+        $score = round($count * 100 / $nbVotes, 2);
+        
+        $poll->setIsEnded(true);
+        
+        if ($score > 50) {
+            $poll->getProject()->setApprovalPoll($poll);
+
+            $this->eventDispatcher->dispatch(AcceptedPollEvent::NAME, new AcceptedPollEvent($poll, $score, $nbVotes));
+        } else {
+            $this->eventDispatcher->dispatch(RejectedPollEvent::NAME, new RejectedPollEvent($poll, $score, $nbVotes));
+        }
+        $this->em->flush();
     }
 }
